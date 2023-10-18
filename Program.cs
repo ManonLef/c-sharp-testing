2c04688e7922afbbe3c97961468df260cb551474
Manon <manonlefkos@gmail.com>
2023-10-18 22:53:41 +0200

    exercise printing literal values

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//////////////// PRINT LITERAL VALUES ////////////////
+
+// Literal String ""
+Console.WriteLine("Hello World!");
+
+// Character literals ''
+Console.WriteLine('b');
+
+// Integer Literals
+Console.WriteLine(123);
+
+// Floating Point Literals - Float Type "float" ~6-9 digits precision
+// indicated with "F" or "f" literal suffix
+Console.WriteLine(0.25F);
+
+// Floating Point Literals - Float Type "double" ~15-17 digits precision
+// The compiler defaults to the double literal when not entering a literal suffix
+Console.WriteLine(0.2625);
+
+// Floating Point Literals - Float Type "decimal" ~28-29 digits precision
+Console.WriteLine(12.39816m);
+
+// Boolean Literals - In C#, they're officially referred to as "bool", but often developers use the term "Boolean".
+Console.WriteLine(true);
+Console.WriteLine(false);
