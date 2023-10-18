9e84ddf5a7ed82bc4541c3f249bc9e441fc78676
Manon <manonlefkos@gmail.com>
2023-10-18 23:12:29 +0200

    set and get values from variables

--- Program.cs
+++ Program.cs
@@ -23,3 +23,26 @@ Console.WriteLine(12.39816m);
 // Boolean Literals - In C#, they're officially referred to as "bool", but often developers use the term "Boolean".
 Console.WriteLine(true);
 Console.WriteLine(false);
+
+//////////////// SET AND GET VALUES FROM VARIABLES ////////////////
+
+// declaring a variable: data type + name
+// variable names: no $ or # allowed, must begin with _ or alphanumeric char, case-sensitive, not be C# keyword
+string firstName;
+
+// variable examples:
+char userOption = 'c';
+int gameScore = 100;
+decimal particlesPerMillion = 1.123m;
+bool processedCustomer = true;
+
+// self note: noticed the terminal giving basic linter style warnings on `dotnet run` like: variable assigned but value never used
+firstName = "Manon";
+Console.WriteLine(firstName, userOption, gameScore, particlesPerMillion, processedCustomer);
+
+// assigning variable value is known as a "set" operation
+// retrieving a value from a variable is also known as a "get" operation
+
+// variables can be reused and reassigned:
+firstName = "Bob";
+Console.WriteLine(firstName);
