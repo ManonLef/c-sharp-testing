61a77ec6f3d18a64ad077dbbdbe482f931880c6a
Manon <manonlefkos@gmail.com>
2023-10-18 23:22:29 +0200

    implicitly typed local variables

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿//////////////// PRINT LITERAL VALUES ////////////////
 
+
 // Literal String ""
 Console.WriteLine("Hello World!");
 
@@ -24,7 +25,11 @@ Console.WriteLine(12.39816m);
 Console.WriteLine(true);
 Console.WriteLine(false);
 
+
+
 //////////////// SET AND GET VALUES FROM VARIABLES ////////////////
+
+
 
 // declaring a variable: data type + name
 // variable names: no $ or # allowed, must begin with _ or alphanumeric char, case-sensitive, not be C# keyword
@@ -46,3 +51,18 @@ Console.WriteLine(firstName, userOption, gameScore, particlesPerMillion, process
 // variables can be reused and reassigned:
 firstName = "Bob";
 Console.WriteLine(firstName);
+
+
+
+//////////////// DECLARE IMPLICITLY TYPED LOCAL VARIABLES ////////////////
+
+
+// Implicitly typed local variable use the `var` keyword followed by variable initialization
+var message = "Hello World!";
+Console.WriteLine(message);
+// The compiler will now imply data type of 'string' because of the assigned value being string. 
+// assigning another data type to `message` will be met with an error
+// var variables need to be initialized straight away, just declaring it will be met with an error. 
+
+
+
