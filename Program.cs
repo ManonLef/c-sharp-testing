20db7637897d40426853ee72746fa172bbb23f34
Manon <manonlefkos@gmail.com>
2023-10-21 23:47:29 +0200

    basic string concatenation

--- Program.cs
+++ Program.cs
@@ -148,3 +148,13 @@ Console.Write(@"c:\invoices");
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 // User command to run an application
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+
+//////////////// STRING CONCATENATION ////////////////
+
+
+string frName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + frName + "!");
+
+
